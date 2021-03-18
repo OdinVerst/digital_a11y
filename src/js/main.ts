129c99ff38144a1a25497794a07cd9fda092e003
Sergey Razdolyanskiy <grafcorp01@gmail.com>
2021-03-18 17:27:17 +0300

    Add banner (html and css) and add swiper

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,1 +1,17 @@
+import Swiper from 'swiper/bundle';
+
+const swiper = new Swiper('.banner-js', {
+	slidesPerView: 1,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	a11y: {
+		prevSlideMessage: 'Показать предыдущий слайд',
+		nextSlideMessage: 'Показать следующий слайд',
+	},
+});
+
+console.log(swiper);
+
 console.log('Connect JS');
